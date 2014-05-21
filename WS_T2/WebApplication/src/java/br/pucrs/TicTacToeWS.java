661105adfe4007875e2fe4d3ff6952da26d696db
Matheus Jardim B <matheusjardimb@gmail.com>
2014-05-20 22:08:07 -0300

    ws created

---
+++ WS_T2/WebApplication/src/java/br/pucrs/TicTacToeWS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.pucrs;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author matheusjardimb
+ */
+@WebService(serviceName = "TicTacToeWS")
+public class TicTacToeWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
